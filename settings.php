aa2e4785eefd06343a4c0dd998f9a74ce311a7f1
ojoven <mikeltorresmail@gmail.com>
2015-03-18 11:06:34 +0100

    refactor

---
+++ settings.php
@@ -0,0 +1,9 @@
+<?php
+
+// We must set an starting point on Sunday
+// because it's the highest point in the panel
+// Format: Y-m-d
+define('STARTING_POINT', '2015-03-22');
+
+// Let's set the message, too
+define('PANEL_MESSAGE', "! c o d e !");

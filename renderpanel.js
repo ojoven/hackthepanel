d63bcb02ea8070f0b2a19c548c2fc8ba20a123e0
ojoven <mikeltorresmail@gmail.com>
2015-03-21 13:26:09 +0100

    more time for screenshot

--- renderpanel.js
+++ renderpanel.js
@@ -38,6 +38,6 @@ page.open(url, function (status) {
             page.render(filePath, { format: extension }); // Phantom creates the images much faster in jpg but avconv creates corrupted video if JPG inputs
             phantom.exit();
 
-        }, 2000);
+        }, 5000);
     }
 });

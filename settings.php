f9020b50b516997e91a42cac4c43fd788cf3bb10
Mikel Torres <michel@majesticmedia.ca>
2015-04-02 14:07:32 +0200

    settings

--- settings.php
+++ settings.php
@@ -3,7 +3,7 @@
 // We must set an starting point on Sunday
 // because it's the highest point in the panel
 // Format: Y-m-d
-define('STARTING_POINT', '2015-04-05');
+define('STARTING_POINT', '2015-03-29');
 
 // Let's set the message, too
 define('PANEL_MESSAGE', "i ♥  c o d e ");

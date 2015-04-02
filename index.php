5c37d068bfc8a2d7c67cbe4077bdfacf44c3fc09
Mikel Torres <michel@majesticmedia.ca>
2015-04-02 14:05:58 +0200

    control access

--- index.php
+++ index.php
@@ -2,5 +2,9 @@
 define('ROOT_PATH', __DIR__ . "/");
 require_once 'app/app.php';
 
+// No browser access
+if (php_sapi_name() != "cli") { exit("Sorry, no access man!"); }
+
+// Let's run!
 $app = new App();
 $app->run();
